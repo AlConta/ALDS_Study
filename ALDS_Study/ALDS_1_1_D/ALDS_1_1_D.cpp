7a8e292832687cfcf7a50aebca4e288e48546113
AlConta <umazaemon@gmail.com>
2020-07-01 16:04:22 +0900

    Add 1-1-D

---
+++ ALDS_Study/ALDS_1_1_D/ALDS_1_1_D.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+const static int MAX = 200000;
+int main()
+{
+    int n{ 0 };
+    cin >> n;
+
+    int maxv = -200000000;
+    int minv = 0;
+    
+    cin >> minv;
+    for (int i = 1; i < n; i++) {
+        int R{ 0 };
+        cin >> R;
+        maxv = max(maxv, R - minv);
+        minv = min(minv, R);
+    }
+    cout << maxv << endl;
+
+    return 0;
+}
